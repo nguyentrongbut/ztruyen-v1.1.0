5c00ded0aacc48e4d84043b19e4e222a03194fa2
nguyentrongbut <cloly031128@gmail.com>
2026-04-03 19:58:57 +0700

    chore: remove image config

--- next.config.ts
+++ next.config.ts
@@ -3,42 +3,6 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
-        // formats: ['image/avif', 'image/webp'],
-        remotePatterns: [
-            // google facebook
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: '**.fbcdn.net',
-                pathname: '/**',
-            },
-
-            // otruyen api
-            {
-                protocol: 'https',
-                hostname: 'img.otruyenapi.com',
-                port: '',
-                pathname: '/uploads/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'sv1.otruyencdn.com',
-                port: '',
-                pathname: '/uploads/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'api.ztruyen.io.vn',
-                port: '',
-                pathname: '/**',
-            },
-        ],
-        // qualities: [60],
         unoptimized: true,
     },
     eslint: {
